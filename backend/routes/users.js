df1e6cc095793042f5801a384a518d5a930681a2
Mock1ng <bayuw1123@gmail.com>
2020-01-21 23:03:09 +0700

    done with backend

---
+++ backend/routes/users.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+let User = require('../models/user.model');
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.json({
+            message: err
+        })
+    }
+})
+
+router.post('/add', async (req, res) => {
+    const user = new User({
+        username: req.body.username
+    })
+    try {
+        const savedUser = await user.save();
+        // res.json('User Added!');
+        res.json(savedUser);
+
+    } catch (err) {
+        res.json({
+            message: err
+        })
+    }
+})
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,10 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+    res.send('Hello there!')
+})
+
 // MIDDLEWARES
 app.use(cors());
 app.use(express.json());
@@ -15,11 +19,21 @@ app.use(express.json());
 // CONNECT THE DATABASE
 const uri = process.env.DB_CONNECTION;
 mongoose.connect(uri, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
 });
 mongoose.connection.once('open', () => {
-    console.log('Database has connected')
-})
+    console.log('Database has been connected')
+});
+
+// Import Routes
+// exercises route
+const exercisesRoute = require('./routes/exercises');
+app.use('/exercises', exercisesRoute);
+// users route
+const usersRoute = require('./routes/users');
+app.use('/users', usersRoute);
 
 // PORT
 app.listen(port, () => console.log(`server has running on: ${port}`))
